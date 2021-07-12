8c5cac73bc421dd6ba23ae3352c6e5feeb351eb7
jason-gkq <guokeqin163@163.com>
2021-07-12 17:39:09 +0800

    去掉废弃代码

--- src/common/core/index.js
+++ src/common/core/index.js
@@ -1,4 +1,3 @@
 export { default as BasePage } from "./basePage";
 export { default as BaseApp } from "./baseApp";
-export { default as Connect } from "./Connect";
 export { ThemeContext, getThemeContext } from "./themeContext";
